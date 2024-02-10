bf6f7225f2d904e2dc11c03de510c824b626914d
Tejas Gupta <131871888+multiverseweb@users.noreply.github.com>
2024-02-10 22:35:49 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function show() {
+    document.getElementById("close").style = "display:block";
+    document.getElementById("navbar").style = "display:block";
+    document.getElementById("stack").style = "display:none";
+}
+function hide() {
+    document.getElementById("close").style = "display:none";
+    document.getElementById("navbar").style = "display:none";
+    document.getElementById("stack").style = "display:block";
+}
